c44cfe0b7b3c5410db2b07274221e80a4b0ea8c5
N3zZa <necrofos9@gmail.com>
2025-07-29 13:36:24 +0300

    fix: changes static dirs storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/shared/ui/**/*.stories.@(js|jsx|ts|tsx)'],
@@ -7,6 +8,6 @@ const config: StorybookConfig = {
     name: '@storybook/nextjs',
     options: {},
   },
-  staticDirs: ['..\\public'],
+  staticDirs: [path.join(__dirname, '../public')],
 };
 export default config;
