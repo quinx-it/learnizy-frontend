c74deeb616f1841f1f45abbc41b2b442671b29b9
InLineA <prokopenya98@gmail.com>
2025-08-04 21:53:02 +0300

    fix: refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import localFont from 'next/font/local';
 import './globals.css';
 import { Header } from '@/shared/components/header';
 import { Footer } from '@shared/components/footer';
-import { Toaster } from '@/shared/ui/sonner/toaster';
+import { Toaster } from '@/shared/ui/toaster/toaster';
 
 export const metadata: Metadata = {
   title: {
