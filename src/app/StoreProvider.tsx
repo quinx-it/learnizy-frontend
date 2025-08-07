cd9ea6e66f685d7c543d0d1a7f1232c5437fc286
InLineA <prokopenya98@gmail.com>
2025-08-07 13:05:23 +0300

    fix: presist loading state

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -3,6 +3,7 @@ import { useRef } from 'react';
 import { Provider } from 'react-redux';
 import { makeStore, AppStore, persistor } from '@/store/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Spinner } from '@/shared/ui/spinner';
 
 export default function StoreProvider({ children }: { children: React.ReactNode }) {
   const storeRef = useRef<AppStore | null>(null);
@@ -12,7 +13,7 @@ export default function StoreProvider({ children }: { children: React.ReactNode 
 
   return (
     <Provider store={storeRef.current}>
-      <PersistGate persistor={persistor} loading={null}>
+      <PersistGate persistor={persistor} loading={<Spinner />}>
         {children}
       </PersistGate>
     </Provider>
