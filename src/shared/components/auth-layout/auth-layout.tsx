8051f6f95e2ea7c0c44af31c014ea86a2fc9e9c6
InLineA <prokopenya98@gmail.com>
2025-08-08 11:16:33 +0300

    fix: redirect from login page

--- src/shared/components/auth-layout/auth-layout.tsx
+++ src/shared/components/auth-layout/auth-layout.tsx
@@ -1,18 +1,24 @@
 'use client'
 import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 import { selectAuth } from '@/store/slices/auth/selectors';
 import { useAppSelector } from '@/shared/hooks/redux';
+import { routes } from '@/shared/constants';
 
 export function AuthLayout({ children }: { children: React.ReactNode }) {
   const router = useRouter();
+  const pathname = usePathname();
   const { accessToken } = useAppSelector(selectAuth);
 
   useEffect(() => {
     if (!accessToken) {
-      router.push('/login');
+      router.push(routes.loginPage);
     }
-  }, [accessToken,router]);
+
+    if (accessToken && pathname === routes.loginPage) {
+      router.push(routes.homePage);
+    }
+  }, [accessToken, router, pathname]);
 
   return accessToken ? children : null;
 }
