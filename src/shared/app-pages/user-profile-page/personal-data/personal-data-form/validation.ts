5bb34b70c2744db3fb2000acaeda088f3dab5025
InLineA <prokopenya98@gmail.com>
2025-08-11 11:26:26 +0300

    fix: personal data form validation

---
+++ src/shared/app-pages/user-profile-page/personal-data/personal-data-form/validation.ts
@@ -0,0 +1,22 @@
+import * as yup from 'yup'
+
+export const personalDataSchema = yup.object({
+  gender: yup
+    .string()
+    .oneOf(['man', 'woman'], 'Выберите "Мужчина" или "Женщина"')
+    .required('Выберите пол'),
+  firstName: yup.string().required('Введите имя'),
+  lastName: yup.string().required('Введите фамилию'),
+  email: yup.string().email('Некорректный email').required('Введите email'),
+  address: yup.string().required('Введите адрес'),
+  phone: yup
+    .string()
+    .matches(
+      /^(\(+375(25|29|33|44)\d{7}|\+7\d{10})$/,
+      'Некорректный номер (РБ: +375XXXXXXXXX, РФ: +7XXXXXXXXXX)'
+    )
+    .required('Введите номер'),
+  birthDate: yup.date().nullable().required('Выберите дату рождения'),
+  country: yup.string().required('Введите страну'),
+  city: yup.string().required('Введите город'),
+})
