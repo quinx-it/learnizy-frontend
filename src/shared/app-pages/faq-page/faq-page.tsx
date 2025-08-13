09cdc39e7569ca77c89aaae0bb02d7b9a5eb962c
N3zZa <necrofos9@gmail.com>
2025-08-13 11:35:19 +0300

    feat: faq page

---
+++ src/shared/app-pages/faq-page/faq-page.tsx
@@ -0,0 +1,28 @@
+import { CardWrapper } from '@/shared/components/card-wrapper';
+import { routes } from '@/shared/constants';
+import { CardAccordion } from '@/shared/ui/accordion';
+import { Breadcrumbs } from '@/shared/ui/breadcrumbs';
+import { Text } from '@/shared/ui/typography';
+import React from 'react';
+import { constants } from './constants';
+
+export const FaqPage = () => {
+  return (
+    <>
+      <Breadcrumbs
+        rootLabel={routes.knowlegeBaseLabel}
+        rootHref={routes.knowlegeBase}
+        items={constants.breadCrumbs}
+      />
+      <CardWrapper className='max-w-full'>
+        <div>
+          <Text variant={'m-bold'} className='text-medium mb-5'>Ответы на популярные вопросы</Text>
+          <div className='bg-gray w-full h-[1px] mb-4' />
+          <div className='w-full'>
+            <CardAccordion className='w-full' items={constants.accordionItems} />
+          </div>
+        </div>
+      </CardWrapper>
+    </>
+  );
+};
--- src/shared/ui/breadcrumbs/constants.ts
+++ src/shared/ui/breadcrumbs/constants.ts
@@ -1,4 +1,8 @@
 export const constants = {
   rootLabel: 'Главная',
+  modulesLabel: 'Структура обучения',
+  baseLabel: 'База знаний',
+  faqLabel: 'FAQ',
+  interviewLabel: 'Вопросы для собеседования',
   rootHref: '/learn',
 };
