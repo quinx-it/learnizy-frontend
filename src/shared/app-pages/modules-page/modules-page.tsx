8773cd3d0ca6f3022b931e2eaf2a2d89ad924cb8
N3zZa <necrofos9@gmail.com>
2025-08-13 11:48:51 +0300

    feat: breadcrumb for the modules page

--- src/shared/app-pages/modules-page/modules-page.tsx
+++ src/shared/app-pages/modules-page/modules-page.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { constants } from './constants';
 import { ModuleCard } from '@/shared/components/module-card';
+import { Breadcrumbs } from '@/shared/ui/breadcrumbs';
+import { routes } from '@/shared/constants';
 
 export const ModulesPage = () => {
   return (
-    <div className="grid grid-cols-2 gap-4">
-      {constants.mockModules.map((module) => (
-        <ModuleCard className="w-full max-w-full" key={module.id} {...module} />
-      ))}
-    </div>
+    <>
+      <Breadcrumbs rootLabel={routes.knowlegeBaseLabel} rootHref={routes.knowlegeBase} rootDescription={'Java Core'} />
+      <div className="grid grid-cols-2 gap-4">
+        {constants.mockModules.map((module) => (
+          <ModuleCard className="w-full max-w-full" key={module.id} {...module} />
+        ))}
+      </div>
+    </>
   );
 };
 
