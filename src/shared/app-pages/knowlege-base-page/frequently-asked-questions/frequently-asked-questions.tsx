d1736d114d6e427298abaf2375ff67d82819f39a
InLineA <prokopenya98@gmail.com>
2025-08-13 12:53:39 +0300

    fix: images

--- src/shared/app-pages/knowlege-base-page/frequently-asked-questions/frequently-asked-questions.tsx
+++ src/shared/app-pages/knowlege-base-page/frequently-asked-questions/frequently-asked-questions.tsx
@@ -3,10 +3,11 @@ import { routes } from '@/shared/constants';
 import { Button } from '@/shared/ui/button';
 import { Text } from '@/shared/ui/typography';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export const FrequentlyAskedQuestions = () => {
   return (
-    <CardWrapper className="flex flex-col gap-4">
+    <CardWrapper className="flex flex-col gap-4 relative overflow-hidden">
       <Text variant="m" className="text-medium">
         Ответы на популярные вопросы
       </Text>
@@ -24,6 +25,8 @@ export const FrequentlyAskedQuestions = () => {
       <Button variant="blue" size="small" asChild className="w-fit">
         <Link href={routes.frequentlyAskedQuestions}>Смотреть ответы</Link>
       </Button>
+
+      <Image src='/images/blue-planet-with-moon.webp' alt='' className='absolute bottom-0 right-0 translate-x-15 translate-y-7 rotate-6' width={160} height={107} />
     </CardWrapper>
   );
 };
--- src/shared/app-pages/knowlege-base-page/interview-questoins/interview-questoins.tsx
+++ src/shared/app-pages/knowlege-base-page/interview-questoins/interview-questoins.tsx
@@ -3,11 +3,12 @@ import { routes } from '@/shared/constants';
 import { Button } from '@/shared/ui/button';
 import { Text } from '@/shared/ui/typography';
 import Link from 'next/link';
+import Image from 'next/image';
 import React from 'react'
 
 export const InterviewQuestions = () => {
   return (
-    <CardWrapper className="flex flex-col gap-4">
+    <CardWrapper className="flex flex-col gap-4 relative overflow-hidden">
         <Text variant="m" className="text-medium">
           Вопросы для собеседования
         </Text>
@@ -26,6 +27,8 @@ export const InterviewQuestions = () => {
         <Button variant="blue" size="small" asChild className='w-fit'>
           <Link href={routes.interviewQuestions}>Перейти к вопросам</Link>
         </Button>
+
+        <Image src='/images/planet-with-disc-blue.webp' alt='' className='absolute bottom-0 right-0 translate-x-12 translate-y-4' width={176} height={88} />
       </CardWrapper>
   )
 }
