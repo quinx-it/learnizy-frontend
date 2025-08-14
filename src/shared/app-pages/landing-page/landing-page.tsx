054c65c1cc1fdcebf83966706a10b1ec84689a10
InLineA <prokopenya98@gmail.com>
2025-08-14 16:40:29 +0300

    feat: section content

--- src/shared/app-pages/landing-page/landing-page.tsx
+++ src/shared/app-pages/landing-page/landing-page.tsx
@@ -1,9 +1,15 @@
 import { Heading } from '@/shared/ui/typography';
+import Link from 'next/link';
+import Image from 'next/image';
+import { HeroSection } from './hero-section';
+import { routes } from '@/shared/constants';
 
 export const LandingPage = () => {
   return (
-    <>
+    <main className="m-0 w-full p-0">
+      <HeroSection />
       <Heading>Landing</Heading>
-    </>
+      <Image src="/images/astronaut1.webp" width={100} height={100} alt="mama" />
+    </main>
   );
 };
