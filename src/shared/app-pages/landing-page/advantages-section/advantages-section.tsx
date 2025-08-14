e3f2104a3787dcf14b4824a8ab51f37f1e7c5b8b
N3zZa <necrofos9@gmail.com>
2025-08-14 17:19:46 +0300

    fix: move info to constans

--- src/shared/app-pages/landing-page/advantages-section/advantages-section.tsx
+++ src/shared/app-pages/landing-page/advantages-section/advantages-section.tsx
@@ -1,47 +1,22 @@
 import { Heading } from '@/shared/ui/typography';
 import React from 'react';
-import { AdvantageCard, CardType } from './advantage-card';
+import { AdvantageCard } from './advantage-card';
 import { SectionContent } from '../section-content';
+import { constants } from './constants';
 
 export const AdvantagesSection = () => {
-  const items = [
-    {
-      title: 'Больше, чем Java',
-      text: 'Мы не ограничиваемся только программированием. В Learnizy вы найдете курсы по маркетингу, логистике, бизнес-аналитике и другим востребованным направлениям',
-      type: CardType.Dark,
-    },
-    {
-      title: 'Больше, чем Java',
-      text: 'Мы не ограничиваемся только программированием. В Learnizy вы найдете курсы по маркетингу, логистике, бизнес-аналитике и другим востребованным направлениям',
-      type: CardType.Light,
-    },
-    {
-      title: 'Больше, чем Java',
-      text: 'Мы не ограничиваемся только программированием. В Learnizy вы найдете курсы по маркетингу, логистике, бизнес-аналитике и другим востребованным направлениям',
-      type: CardType.Dark,
-    },
-    {
-      title: 'Больше, чем Java',
-      text: 'Мы не ограничиваемся только программированием. В Learnizy вы найдете курсы по маркетингу, логистике, бизнес-аналитике и другим востребованным направлениям',
-      type: CardType.Light,
-    },
-  ];
-
   return (
     <SectionContent className="bg-[#B7E3F0]">
-      <div className="max-w-[1140px] mx-auto px-4">
+      <div className="mx-auto max-w-[1140px] px-4">
         <div className="w-full px-16">
           <Heading variant={'7xl'} className="mb-2">
-            Почему выбирают нас?
+            {constants.title}
           </Heading>
-          <Heading className="mb-30 max-w-[750px]">
-            От практики с реальными кейсами до поддержки менторов – всё, чтобы вы уверенно прошли
-            техническое интервью.
-          </Heading>
+          <Heading className="mb-30 max-w-[750px]">{constants.description}</Heading>
         </div>
 
         <div className="mx-auto grid w-full grid-cols-1 gap-6 lg:grid-cols-2">
-          {items.map((item, index) => (
+          {constants.cards.map((item, index) => (
             <AdvantageCard key={index} {...item} />
           ))}
         </div>
