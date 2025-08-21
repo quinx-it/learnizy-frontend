fb1e0e237bba3750bd365e7acc3ccac92f8b61ca
InLineA <prokopenya98@gmail.com>
2025-08-21 14:59:44 +0300

    fix: refactor

--- src/shared/lib/utils.ts
+++ src/shared/lib/utils.ts
@@ -5,12 +5,6 @@ export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
 
-export const getOrderClass = (index: number) => {
-    if (index === 2) return 'min-[640px]:max-xl:order-4';
-    if (index === 3) return 'min-[640px]:max-xl:order-3';
-    return `${index + 1}`;
-};
-
 
 
 import { jwtDecode } from 'jwt-decode'
