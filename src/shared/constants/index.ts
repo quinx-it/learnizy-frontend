5fde2c63b927e4f45a1d4c18383f7f8e7de36ea9
InLineA <prokopenya98@gmail.com>
2025-08-22 12:02:05 +0300

    fix: merge dev

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,6 +1,3 @@
 export { routes } from './routes';
-<<<<<<< HEAD
 export { globalConstants } from './constants'
-=======
 export { defaultPage } from './routes'
->>>>>>> 99a9c49ed254db38616e2340df7ad1852817ce6c
