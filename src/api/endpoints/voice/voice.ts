949d9aba648009780050ce8e1fdb14a1a8b3fcdc
Vadim <vzeldak07@gmail.com>
2025-10-08 16:40:16 +0300

    fix: import VoiceData from correct module

--- src/api/endpoints/voice/voice.ts
+++ src/api/endpoints/voice/voice.ts
@@ -1,9 +1,9 @@
 import { api } from '@/api';
-import type { VoiceData, FileUploadResponse } from './index';
+import type { VoiceDataType, FileUploadResponseType } from './index';
 
 export const voice = api.injectEndpoints({
   endpoints: (builder) => ({
-    uploadVoice: builder.mutation<FileUploadResponse, VoiceData>({
+    uploadVoice: builder.mutation<FileUploadResponseType, VoiceDataType>({
       query: (body) => ({
         url: '/file-storage/upload',
         method: 'POST',
