94910d05a9f8461d46975d35875ee9f750786a3f
Vadim <vzeldak07@gmail.com>
2025-10-20 17:55:58 +0300

    fix: auth remove unused useEffect import

--- src/shared/components/auth/auth-form/auth-form.tsx
+++ src/shared/components/auth/auth-form/auth-form.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { AuthFormValues, formSchema } from './validation';
