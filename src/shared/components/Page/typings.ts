57042560106f907d09a4f0fd757908a26c02bfe2
Vadim <vzeldak07@gmail.com>
2025-10-31 23:41:37 +0300

    fix: interface declaring no members

--- src/shared/components/Page/typings.ts
+++ src/shared/components/Page/typings.ts
@@ -1,4 +1,6 @@
 import { PropsWithChildren } from 'react';
 import { IHeadProps } from '@/shared/components/Head/typings';
 
-export interface IPageProps extends PropsWithChildren<IHeadProps> {}
+export interface IPageProps extends PropsWithChildren, IHeadProps {
+  seo?: Record<string, Omit<IHeadProps, 'seo'>>;
+}
