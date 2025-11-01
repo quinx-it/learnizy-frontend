b59c4e684fc05ab3f1173404b18d07435878afc8
Vadim <vzeldak07@gmail.com>
2025-11-02 00:52:13 +0300

    fix: errors

--- src/app/(application)/(auth)/ForgotPassword/page.tsx
+++
@@ -1,8 +0,0 @@
-import { ForgotPasswordPage } from '@/shared/AppPages/AuthPages/ForgotPasswordPage';
-import React from 'react';
-
-function ForgotPassword() {
-  return <ForgotPasswordPage />;
-}
-
-export default ForgotPassword;
