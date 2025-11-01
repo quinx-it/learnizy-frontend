7fc6564eb4b274030d7bdbd77979b39e6e0b301e
Vadim <vzeldak07@gmail.com>
2025-11-02 00:35:23 +0300

    refactor: name files

---
+++ src/app/(application)/(auth)/ForgotPassword/page.tsx
@@ -0,0 +1,8 @@
+import { ForgotPasswordPage } from '@/shared/AppPages/AuthPages/ForgotPasswordPage';
+import React from 'react';
+
+function ForgotPassword() {
+  return <ForgotPasswordPage />;
+}
+
+export default ForgotPassword;
