993329176c2502d7b7ec730d12d3f5ec3b5a227e
Vadim <vzeldak07@gmail.com>
2025-11-02 01:35:56 +0300

    fix: name app

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
-import path from 'path';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'standalone',
   images: {
-    domains: ['encrypted-tbn0.gstatic.com'],
-  },
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
-    return config;
+    domains: ['encrypted-tbn0.gstatic.com'], 
   },
 };
 
