ddca15fd573d872763deee5d2b502a29448e934b
ser12500 <kerget95@gmail.com>
2025-12-09 15:03:55 +0300

    convert

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
     output: 'standalone',
     images: {
         domains: ['encrypted-tbn0.gstatic.com'],
+        formats: ['image/webp', 'image/avif'],
+        dangerouslyAllowSVG: true,
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     },
 };
 
