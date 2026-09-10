0e91bbf2901a657c6066a25cbfb8ced5206fa799
ser12500 <kerget95@gmail.com>
2026-09-10 20:27:25 +0300

    refactor(routes): switch user-facing urls to kebab-case

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -95,7 +95,7 @@ const DYNAMIC_USER_ROUTES = [
   /^\/learn\/courses\/\d+\/modules\/\d+\/\d+\/retelling$/,
   /^\/learn\/exams\/\d+\/\d+\/test$/,
   /^\/learn\/exams\/\d+\/\d+\/result$/,
-  /^\/learn\/aiAssistant\/chat\/[^/]+$/,
+  /^\/learn\/ai-assistant\/chat\/[^/]+$/,
 ];
 
 const PUBLIC_ROUTES = [
